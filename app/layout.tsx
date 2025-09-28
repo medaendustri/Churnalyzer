4964ecc8d946dd41b5bc4c180e26f0999f89952b
medaendustri <fretzclip@gmail.com>
2025-09-28 17:02:15 +0300

    analy

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,8 @@ export default function RootLayout({
       <body className="antialiased">
         <Header />
         {children}
+        <Analytics />
+
         <Footer />
       </body>
     </html>
